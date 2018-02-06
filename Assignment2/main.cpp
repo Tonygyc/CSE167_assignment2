74e202e1c15c180a78406b20efeacf07b4313daa
Luke Rohrer <lrohrer@ucsd.edu>
2018-02-06 14:58:34 -0800

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Luke Rohrer on 2/6/18.
+//  Copyright © 2018 Luke Rohrer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
